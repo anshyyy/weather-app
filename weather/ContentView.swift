1afe0d7d0c9d6eb34c579b5f787a3a47445c5a70
anshyyy <anshuman9998@gmail.com>
2024-07-31 11:56:06 +0530

    initial commit

---
+++ weather/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by Anshuman Sharma on 31/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .padding()
+        }
+}
+
+
+struct ContentView_Previews:PreviewProvider{
+    static var previews: some View{
+        ContentView()
+    }
+}
