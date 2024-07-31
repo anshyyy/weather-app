535683cd2ca2225315820c7ab1d68c1e410e59f7
anshyyy <anshuman9998@gmail.com>
2024-07-31 13:15:20 +0530

    refactoring 101

--- weather/ContentView.swift
+++ weather/ContentView.swift
@@ -10,21 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack(content: {
-            LinearGradient(
-                gradient: Gradient(
-                    colors: [
-                        Color.blue,
-                        Color.blue.opacity(0.3)
-                       // Color.white
-                    ]
-                ),
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing
-            )
-            .edgesIgnoringSafeArea(
-                /*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/
-            )
-            
+            BackgroundView(colors: [Color.blue,Color.blue.opacity(0.7)])
             VStack(
                 content:{
                     Text("Cupertino, CA")
@@ -42,11 +28,11 @@ struct ContentView: View {
                             .foregroundColor(.white)
                     }.padding(.bottom,40)
                     
-                   
+                    
                     
                     HStack(spacing: 20){
-                    
-                       DayForecastView(day: "TUE", imageName: "sun.horizon", temperature: "24°")
+                        
+                        DayForecastView(day: "TUE", imageName: "sun.horizon", temperature: "24°")
                         DayForecastView(day: "WED", imageName: "moon.dust", temperature: "23°")
                         DayForecastView(day: "THU", imageName: "sun.dust", temperature: "25°")
                         DayForecastView(day: "FRI", imageName: "sun.rain", temperature: "29°")
@@ -107,5 +93,23 @@ struct DayForecastView: View {
 struct ContentView_Previews:PreviewProvider{
     static var previews: some View{
         ContentView()
+    }
+}
+
+struct BackgroundView: View {
+    
+    var colors: [Color]
+    
+    var body: some View {
+        LinearGradient(
+            gradient: Gradient(
+                colors: colors
+            ),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+        .edgesIgnoringSafeArea(
+            /*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/
+        )
     }
 }
